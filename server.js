1b4cb6e7ba877d7d09fe2d44d63bcccd7b45aa0b
JohanMazzoleni <85082764+JohanMazzoleni@users.noreply.github.com>
2021-10-02 08:55:27 +0200

    feat(back-end): add mongoose

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
+
+require("dotenv").config();
+
+mongoose.connect(process.env.CREDENTIAL, function (err) {
+    if (err) throw err;
+    console.log("Successfully connected");
+});
 
 const auth = require("./routes/auth");
 
