f238578496d5c642e72d5051a995ae1abdd2261d
JohanMazzoleni <85082764+JohanMazzoleni@users.noreply.github.com>
2021-10-04 05:59:34 +0200

    feat(back-end): add token verification

---
+++ middleware/verifyToken.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const models = {
+    UsersModel: require("../models/Users"),
+}
+
+module.exports = function (req, res, next) {
+    try {
+        let bearer = req.headers.authorization.split(" ")[1];
+        let decodeToken = jwt.verify(bearer, process.env.JWT_SECRET);
+
+        models.UsersModel.findOne({
+            _id: decodeToken.userId,
+        }).then(function () {
+            next();
+        })
+            .catch(function () {
+                res.status(401);
+                res.json({
+                    status: false,
+                })
+            })
+    }
+    catch (err) {
+        res.status(401);
+        res.json({
+            status: false,
+        })
+    }
+}
