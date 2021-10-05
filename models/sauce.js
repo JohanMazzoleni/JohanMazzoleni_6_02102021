a4e0008bc6051e1bf6c1dd494569e8a7559233ca
JohanMazzoleni <85082764+JohanMazzoleni@users.noreply.github.com>
2021-10-06 00:04:07 +0200

    fix(back-end): edit sauce model

--- models/sauce.js
+++ models/sauce.js
@@ -3,36 +3,47 @@ const mongoose = require("mongoose");
 var schema = mongoose.Schema({
     userId: {
         type: String,
+        required: 1,
     },
     name: {
-        type: String
+        type: String,
+        required: 1,
     },
     manufacturer: {
         type: String,
+        required: 1,
     },
     description: {
         type: String,
+        required: 1,
     },
     mainPepper: {
         type: String,
+        required: 1,
     },
     imageUrl: {
         type: String,
+        required: 1,
     },
     heat: {
         type: Number,
+        required: 1,
     },
     likes: {
         type: Number,
+        required: 1,
     },
     dislikes: {
         type: Number,
+        required: 1,
     },
     usersLiked: {
         type: Array,
+        required: 1,
     },
     usersDisliked: {
         type: Array,
+        required: 1,
     }
 });
 
